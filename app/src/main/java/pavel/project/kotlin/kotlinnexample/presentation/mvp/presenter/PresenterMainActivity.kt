5fd3bad81cf52a59a6e9afef52f6f4d893478208
upward@tut.by <381PAVELm>
2018-02-12 22:23:09 +0300

    fix presenter

--- app/src/main/java/pavel/project/kotlin/kotlinnexample/presentation/mvp/presenter/PresenterMainActivity.kt
+++ app/src/main/java/pavel/project/kotlin/kotlinnexample/presentation/mvp/presenter/PresenterMainActivity.kt
@@ -2,8 +2,6 @@ package pavel.project.kotlin.kotlinnexample.presentation.mvp.presenter
 
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
-
-import pavel.project.kotlin.kotlinnexample.data.mapper.UserEntityDataMapper
 import pavel.project.kotlin.kotlinnexample.domain.biusness.NetworkInteractor
 import pavel.project.kotlin.kotlinnexample.presentation.mvp.ui.view.IViewMain
 import pavel.project.kotlin.kotlinnexample.rxschedulers.rx.RxSchedulers
@@ -12,24 +10,12 @@ import pavel.project.kotlin.kotlinnexample.rxschedulers.rx.RxSchedulers
  * Created by pavel on 20.10.2017.
  */
 @InjectViewState
-class PresenterMainActivity : MvpPresenter<IViewMain>() {
-    private val networkInteractor: NetworkInteractor? = null
-    private val rxSchedulers: RxSchedulers? = null
-
-    init {
-
-    }
+class PresenterMainActivity( private val networkInteractor: NetworkInteractor,
+                             private val rxSchedulers: RxSchedulers) : MvpPresenter<IViewMain>() {
 
     fun onLoadData(){
-        networkInteractor?.getAllProfile()?.subscribeOn(rxSchedulers?.io())
-                .observeOn(rxSchedulers?.androidThread()).subscribe
-           /* iViewMainActivity.showList(lists);
-            iViewMainActivity.hideProgress();
-
-        }, throwable -> iViewMainActivity.showError(throwable.toString()));
-
-        iViewMainActivity.showError(String.valueOf(R.string.check_your_internet_connection));
-        iViewMainActivity.hideProgress();
-        iViewMainActivity.showErrorInActivity();*/
+        networkInteractor.getAllProfile().subscribeOn(rxSchedulers.io())
+                .observeOn(rxSchedulers.androidThread()).subscribe()
+//todo
     }
 }
